9bf273f4317c6b35152fccf8ceb89b7248ab8b31
Bilal <bilalfaisalather19@gmail.com>
2025-04-30 23:07:49 +0500

    adding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,33 @@
 import React, { useState, useEffect } from "react";
+import sir1 from "src/assets/sir.jpg";
+import sir2 from "src/assets/sir.jpg";
+import sir3 from "src/assets/sir.jpg";
 
 const Header = () => {
-  const images = [
-    "src/assets/sir.jpg", // Replace with actual image paths
-    "src/assets/sir.jpg",
-    "src/assets/sir.jpg",
-  ];
-  // State to track the current background image index
+  const images = [sir1, sir2, sir3];
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  // Use useEffect to change the image index at a regular interval
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImageIndex((prevIndex) =>
         prevIndex === images.length - 1 ? 0 : prevIndex + 1
       );
-    }, 3000); // Change image every 3 seconds
-
-    // Cleanup the interval on component unmount
+    }, 3000);
     return () => clearInterval(interval);
-  }, [images.length]);
+  }, []);
 
   return (
     <div
       className="relative bg-cover bg-center h-[30vh] md:h-[60vh]"
       style={{
-        backgroundImage: `url('${images[currentImageIndex]}')`,
+        backgroundImage: `url(${images[currentImageIndex]})`,
       }}
     >
-
-      {/* Overlay for better text readability */}
       <div className="h-full flex items-center justify-center bg-black bg-opacity-50">
+        {/* Optional content */}
       </div>
     </div>
   );
 };
 
-export default Header
+export default Header;
